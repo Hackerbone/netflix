286995ece72d0ae1079ea150a54daf9795c369bd
Sitaraman S <sitaraman.raghav.india@gmail.com>
2018-08-21 22:51:48 +0530

    Second Push
    Files added

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  
+  
+  $("h1").slideUp(0);
+  $("h1").fadeIn(1500);
+  
+  $("h3").slideUp(0);
+  $("h3").fadeIn(1500);
+  
+  $("button").slideUp(0);
+  $("button").fadeIn(2000);
+  
+  $("button").onclick($("h1").slideUp(2000););
+  
+  
+});
